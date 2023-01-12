b448c1522467c64bcc18eaea217a02dd82b12649
Joshy <joshuamcrawford999@gmail.com>
2023-01-11 19:25:09 -0700

    Learned.List

---
+++ LearnedList.js
@@ -0,0 +1,74 @@
+//This File Contains All the things I have learned taking this course//
+
+{
+$1 ("<hr>") // This is a horizontal line in HTML
+    // [Example] 
+    //<hr size="3" noshade>
+
+.
+$2 ("h1-h6") // 
+    // [Note] 
+    // h1-h6 are the different sizes of headers in HTML
+    // h1-h6 is also a hierarchy of importance
+.
+$3 (<a href="") // 
+    // [Example] 
+    // <p>I love weed, I buy from <a href="https://zenleafdispensaries.com/">Zen Leaf Dispensary</a></p>
+.
+$4 ("") // 
+    // [Example] 
+    //
+
+
+.
+$5 ("") // 
+    // [Example] 
+    //
+.
+$6 ("") // 
+    // [Example] 
+    //
+.
+$7 ("") // 
+    // [Example] 
+    //
+
+.
+$8 ("") // 
+    // [Example] 
+    //
+.
+$9 ("") // 
+    // [Example] 
+    //
+.
+$10 ("") // 
+    // [Example] 
+    //
+
+.
+$11 ("") // 
+    // [Example] 
+    //
+.
+$12 ("") // 
+    // [Example] 
+    //
+.
+$13 ("") // 
+    // [Example] 
+    //
+
+.
+$14 ("") // 
+    // [Example] 
+    //
+.
+$15 ("") // 
+    // [Example] 
+    //
+.
+$16 ("") // 
+    // [Example] 
+    //
+}
